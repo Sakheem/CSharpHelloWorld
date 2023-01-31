07ae38ec140b45f68d0e344019f122cdba2eefaa
husss <husss@LAPTOP-IFE5P1TM>
2023-01-30 20:08:30 -0500

    Add project files.

---
+++ HelloWorldProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace HelloWorldProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World, I am Sahim Hussaini");
+            ReadKey();
+        }
+    }
+}
